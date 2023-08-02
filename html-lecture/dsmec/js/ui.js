4a6973e59c0f415b562648cd84e6acad96df0693
jjang051hta <jjang051hta@gmail.com>
2023-08-02 15:32:03 +0900

    swiper end

---
+++ html-lecture/dsmec/js/ui.js
@@ -0,0 +1,22 @@
+// js는  인터프리팅 언어이다. 컴파일 과정을 거치지 않는다.
+//alert("hello javascript");
+//console.log("hello world"); //메서드
+//console.log(19090 + 48934);
+const gnb = document.querySelector(".gnb > ul");
+const header = document.querySelector(".header");
+const btnClose = document.querySelector(".popup .btns .close");
+console.log("🚀 ~ file: ui.js:8 ~ btnClose:", btnClose);
+const popup = document.querySelector(".popup");
+
+gnb.addEventListener("mouseenter", function () {
+  console.log("gnb에 마우스 올라온걸 체크(감지)할 수 있다.");
+  header.classList.add("open");
+});
+gnb.addEventListener("mouseleave", function () {
+  console.log("gnb에 마우스 올라온걸 체크(감지)할 수 있다.");
+  header.classList.remove("open");
+});
+btnClose.addEventListener("click", function () {
+  //console.log("click");
+  popup.classList.add("hidden");
+});
--- html-lecture/dsmec/js/visual.js
+++ html-lecture/dsmec/js/visual.js
@@ -1,1 +1,14 @@
-const swiper = new Swiper(".main-visual", {});
+//json
+
+const swiper = new Swiper(".main-visual", {
+  effect: "slide",
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  speed: 1000,
+  pagination: {
+    el: ".main-visual .pagination",
+    type: "bullets",
+  },
+});
