f486d304bc909a1e1f91c928e4700d55608e09ba
jjang051hta <jjang051hta@gmail.com>
2023-08-23 09:15:00 +0900

    quiz0605 end

--- java-lecture/ch06/src/StudentTest.java
+++ java-lecture/ch06/src/StudentTest.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -55,27 +58,51 @@ class Student02 {
 
 public class StudentTest {
     public static void main(String[] args) {
-        HashMap<String, Student02> studentList = new HashMap<>();
-        studentList.put("장성호", new Student02("장성호", "java", "01", 3.0));
-        studentList.put("장동건", new Student02("장동건", "python", "01", 3.0));
-        studentList.put("정형돈", new Student02("정형돈", "c#", "01", 3.0));
-        studentList.put("유재석", new Student02("장성호", "node", "01", 3.0));
-        // Set<String> keys = studentList.keySet();
-        // java.util.Iterator<String> it = keys.iterator();
-        // while (it.hasNext()) {
-        // System.out.println(studentList.get(it.next()).toString());
-        // }
+        List<Student02> studentList = new ArrayList<>();
+        studentList.add(new Student02("장성호", "java", "01", 3.0));
+        studentList.add(new Student02("장동건", "python", "01", 3.0));
+        studentList.add(new Student02("정형돈", "c#", "01", 3.0));
+        studentList.add(new Student02("정준하", "node", "01", 3.0));
+
+        Iterator<Student02> it = studentList.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.println("학생 이름을 입력하시오");
             String name = scanner.nextLine();
             if (name.equals("그만"))
                 return;
-            if (studentList.get(name) == null) {
-                System.out.println("없는 학생입니다. 우리 학교에 10년째 다니는 다리없는 콩콩콩 뛰어 다니는 애 있습니다.");
-            } else {
-                System.out.println(studentList.get(name).toString());
+            for (int i = 0; i < studentList.size(); i++) {
+                Student02 student = studentList.get(i);
+                if (student.getName().equals(name)) {
+                    System.out.println(student);
+                }
             }
         }
+
+        // HashMap<String, Student02> studentList = new HashMap<>();
+        // studentList.put("장성호", new Student02("장성호", "java", "01", 3.0));
+        // studentList.put("장동건", new Student02("장동건", "python", "01", 3.0));
+        // studentList.put("정형돈", new Student02("정형돈", "c#", "01", 3.0));
+        // studentList.put("유재석", new Student02("장성호", "node", "01", 3.0));
+        // // Set<String> keys = studentList.keySet();
+        // // java.util.Iterator<String> it = keys.iterator();
+        // // while (it.hasNext()) {
+        // // System.out.println(studentList.get(it.next()).toString());
+        // // }
+        // Scanner scanner = new Scanner(System.in);
+        // while (true) {
+        // System.out.println("학생 이름을 입력하시오");
+        // String name = scanner.nextLine();
+        // if (name.equals("그만"))
+        // return;
+        // if (studentList.get(name) == null) {
+        // System.out.println("없는 학생입니다. 우리 학교에 10년째 다니는 다리없는 콩콩콩 뛰어 다니는 애 있습니다.");
+        // } else {
+        // System.out.println(studentList.get(name).toString());
+        // }
+        // }
     }
 }
