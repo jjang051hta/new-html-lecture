2896b3f37d5b53e1243a5a05a8672306d0f19d40
jjang051hta <jjang051hta@gmail.com>
2023-08-25 12:11:38 +0900

    swing end

--- java-lecture/ch08/src/MultiLayout.java
+++ java-lecture/ch08/src/MultiLayout.java
@@ -8,16 +8,43 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.Border;
 
 class NorthPanel extends JPanel {
     NorthPanel() {
-        setLayout(new FlowLayout(FlowLayout.CENTER, 10, 0));
+        setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
         setOpaque(true);
         setBackground(Color.LIGHT_GRAY);
         add(new JButton("열기"));
         add(new JButton("닫기"));
         add(new JButton("나가기"));
+    }
+}
+
+class SouthPanel extends JPanel {
+    SouthPanel() {
+        setLayout(new FlowLayout(FlowLayout.LEFT, 10, 10));
+        setOpaque(true);
+        setBackground(Color.LIGHT_GRAY);
+        add(new JButton("word input"));
+        add(new JTextField(15));
+
+    }
+}
+
+class CenterPanel extends JPanel {
+    CenterPanel() {
+        setLayout(null);
+        setOpaque(true);
+        setBackground(Color.YELLOW);
+        for (int i = 0; i < 20; i++) {
+            JLabel label = new JLabel("*");
+            label.setSize(20, 20);
+            label.setForeground(Color.RED);
+            label.setLocation((int) (Math.random() * 200) + 50, (int) (Math.random() * 200) + 50);
+            add(label);
+        }
     }
 }
 
@@ -29,6 +56,8 @@ public class MultiLayout extends JFrame {
         Container contenPane = getContentPane();
         contenPane.setLayout(new BorderLayout());
         contenPane.add(new NorthPanel(), BorderLayout.NORTH);
+        contenPane.add(new CenterPanel(), BorderLayout.CENTER);
+        contenPane.add(new SouthPanel(), BorderLayout.SOUTH);
         // contenPane.setLayout(null);
 
         setSize(300, 300);
