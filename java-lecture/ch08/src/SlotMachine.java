e1e9d724152daad9282832650e867e6ca2af83b7
jjang051hta <jjang051hta@gmail.com>
2023-08-25 17:44:54 +0900

    slot machine

--- java-lecture/ch08/src/SlotMachine.java
+++ java-lecture/ch08/src/SlotMachine.java
@@ -4,13 +4,18 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Random;
+import java.util.Set;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class SlotMachine extends JFrame {
     List<JLabel> labelList = new ArrayList<>();
+    int nums[] = new int[3];
+    Random random = new Random();
 
     SlotMachine() {
         setTitle("SLOT MACHINE");
@@ -18,7 +23,11 @@ public class SlotMachine extends JFrame {
 
         Container contentPane = getContentPane();
         contentPane.setLayout(null);
-
+        JLabel resultLabel = new JLabel();
+        resultLabel.setSize(300, 50);
+        resultLabel.setLocation(0, 250);
+        resultLabel.setText("경기를 시작합니다.");
+        contentPane.add(resultLabel);
         for (int i = 0; i < 3; i++) {
             JLabel label = new JLabel();
             label.setOpaque(true);
@@ -29,11 +38,36 @@ public class SlotMachine extends JFrame {
             label.setText("0");
             label.setHorizontalAlignment(JLabel.CENTER);
             contentPane.add(label);
+            labelList.add(label);
         }
         contentPane.addKeyListener(new KeyAdapter() {
             public void keyPressed(KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_ENTER) {
                     System.out.println("enter");
+                    // int ran01 = random.nextInt(5);
+                    // labelList.get(0).setText("" + ran01);
+                    // int ran02 = random.nextInt(5);
+                    // labelList.get(1).setText("" + ran02);
+                    // int ran03 = random.nextInt(5);
+                    // labelList.get(2).setText("" + ran03);
+                    // if (ran01 == ran02 && ran02 == ran03) {
+                    // System.out.println("빙고");
+                    // }
+                    Set<Integer> set = new HashSet<>();
+                    // Set은 중복을 허용하지 않음...
+                    for (int i = 0; i < labelList.size(); i++) {
+                        int ran = random.nextInt(2);
+                        labelList.get(i).setText("" + ran);
+                        // nums[i] = ran;
+                        set.add(ran);
+                    }
+                    if (set.size() == 1) {
+                        System.out.println("빙고");
+                        resultLabel.setText("빙고");
+                    } else {
+                        resultLabel.setText("땡");
+                    }
+
                 }
             }
 
