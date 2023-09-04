94bd7d1d01c985000b81c6ab67bcfa1413f8b1a1
jjang051hta <jjang051hta@gmail.com>
2023-09-04 09:54:02 +0900

    다중 채팅

--- java-lecture/ch12/src/chatting/Client.java
+++ java-lecture/ch12/src/chatting/Client.java
@@ -5,6 +5,13 @@ import java.awt.Color;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -18,14 +25,19 @@ import javax.swing.border.Border;
 
 // 다중 채팅...
 // 이름을 입력받아서 window열리면서 
-public class Client extends JFrame {
-    private String chattingName = null;
+public class Client extends JFrame implements Runnable {
+    String chattingName = null;
     JLabel nickLabel = new JLabel();
     JTextArea textArea = null;
     JTextField textField = null;
     JButton btn = null;
+    Socket socket = null;
+    private BufferedReader bufferedReader = null;
+    private BufferedWriter bufferedWriter = null;
 
     Client() {
+        // Thread 만들어서...
+        // 서버에서 받아보기...
         chattingName = JOptionPane.showInputDialog(this, "닉네임을 쓰세요.", "닉네임", JOptionPane.YES_NO_OPTION);
         // System.out.println(chattingName);
         if (chattingName.length() == 0 || chattingName == null) {
@@ -50,20 +62,55 @@ public class Client extends JFrame {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                textArea.append(chattingName + " : " + textField.getText() + "\n");
-                textField.setText("");
-            }
+                // textArea.append(chattingName + " : " + textField.getText() + "\n");
+                // 누가 글을 썻는지 와 내용
+                try {
+                    bufferedWriter.write(chattingName + "#" + textField.getText() +
+                            "\n");
+                    bufferedWriter.flush();
+                    // 이걸 서버에서 받는다.
+                    // 받아서 접속된 클라이언트에 싹다 뿌려준다.
+                    textField.setText("");
 
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+
+            }
         });
         this.setTitle("멀티 채팅");
         this.pack();
         this.setResizable(false);
-        // this.setSize(400, 300);
         this.setVisible(true);
 
+        try {
+            socket = new Socket("localhost", 9995);
+            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            Thread thread = new Thread(this);
+            thread.start();
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
         new Client();
+    }
+
+    @Override
+    public void run() {
+        String message = null;
+        while (true) {
+            try {
+                message = bufferedReader.readLine();
+                System.out.println("서버에서 클라이언트 전부에게 뿌린 메세지 ==" + message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- java-lecture/ch12/src/chatting/Server.java
+++ java-lecture/ch12/src/chatting/Server.java
@@ -1,5 +1,98 @@
 package chatting;
 
-public class Server {
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+public class Server extends JFrame {
+    private JTextArea log = new JTextArea(20, 30);
+    private JLabel userLabel = new JLabel();
+    private ArrayList<ClientSocketThread> clientSocketList = null;
+    private Socket socket = null;
+    private ServerSocket serverSocket = null;
+
+    public Server() {
+        super("멀티 채팅");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 프레임 종료 버튼(X)을 클릭하면 프로그램 종료
+        Container c = getContentPane();
+        log.setBackground(Color.GREEN);
+        c.add(new JScrollPane(log), BorderLayout.CENTER);
+        userLabel.setText("현재 접속자수는 0명 입니다.");
+        c.add(userLabel, BorderLayout.SOUTH);
+        this.pack();
+        this.setResizable(false);
+        setVisible(true);
+        clientSocketList = new ArrayList<>();
+        try {
+            serverSocket = new ServerSocket(9995);
+            userLabel.setText("서버 동작중입니다.");
+            ClientSocketThread clientSocketThread = null;
+            while (true) {
+                socket = serverSocket.accept();
+                log.append("클라이언트 접속 \n");
+                clientSocketThread = new ClientSocketThread();
+                clientSocketList.add(clientSocketThread);
+                clientSocketThread.start();
+            }
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    class ClientSocketThread extends Thread {
+        private BufferedReader bufferedReader = null;
+        private BufferedWriter bufferedWriter = null;
+        private String message = null;
+
+        @Override
+        public void run() {
+            try {
+                bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                while (true) {
+                    message = bufferedReader.readLine();
+                    // System.out.println("client에서 넘어온 message = " + message);
+                    broadCasting(message);
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        public void send(String message) {
+            try {
+                bufferedWriter.write(message + "\n");
+                bufferedWriter.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void broadCasting(String message) {
+            // clientSocketList에 있는 곳에다가 전부 전송해주기...
+            for (int i = 0; i < clientSocketList.size(); i++) {
+                ClientSocketThread client = clientSocketList.get(i);
+                client.send(message);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Server();
+    }
 }
